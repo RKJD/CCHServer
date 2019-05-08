9db188da574a2902f3b20ce338fa1e473244494d
Raul Barrero <rbarrero1998@gmail.com>
2019-05-08 12:01:21 +0200

    Servidor #1

--- src/main/java/com/xokundevs/cchmavenserver/Main.java
+++ src/main/java/com/xokundevs/cchmavenserver/Main.java
@@ -1,15 +1,44 @@
 package com.xokundevs.cchmavenserver;
 
-/**
- *
- * @author xokundevs
- */
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
 
     private static final int puerto = 55555;
 
     public static void main(String[] args) {
+        try {
+            ServerSocket ssk = new ServerSocket(puerto);
+            System.out.println("Escuchando");
 
+            while (true) {
+                Socket sk = ssk.accept();
+                Servidor ser = new Servidor(sk);
+                ser.start();
+            }
+
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+    }
+
+    static class Servidor extends Thread {
+
+        Socket sk;
+
+        Servidor(Socket sk) {
+            this.sk = sk;
+        }
+
+        @Override
+        public void run() {
+
+        }
     }
 }
