bde9cd0219593b9ffeee4645ca1b3de4b5457fa0
Raul Barrero <rbarrero1998@gmail.com>
2019-05-08 10:54:05 +0200

    Inicial Commit

---
+++ src/main/java/com/xokundevs/cchmavenserver/Main.java
@@ -0,0 +1,15 @@
+package com.xokundevs.cchmavenserver;
+
+/**
+ *
+ * @author xokundevs
+ */
+
+public class Main {
+
+    private static final int puerto = 55555;
+
+    public static void main(String[] args) {
+
+    }
+}
